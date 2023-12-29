ad7a37d3e7a2ece2e973d0846dc7180535e4356b
maxlem24 <lemaitre.maxime2004@gmail.com>
2023-12-29 14:29:41 +0100

    Initial Commit

---
+++ Carivederci/CarivederciApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarivederciApp.swift
+//  Carivederci
+//
+//  Created by Maxime on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarivederciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carivederci/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Carivederci
+//
+//  Created by Maxime on 29/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
