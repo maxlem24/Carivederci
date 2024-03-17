a4fe64d3a2b09bad2bf8caf6035d81bcc220621f
maxlem24 <lemaitre.maxime2004@gmail.com>
2024-03-17 22:48:09 +0100

    Finalement on a fait la teamView, faudra rédiger les paragraphes, mais demander à la comm pour ça

--- Carivederci/InfosListe/TeamsView.swift
+++ Carivederci/InfosListe/TeamsView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct TeamsView: View {
     var body: some View {
-        Text("Teams")
+        GeometryReader{
+            geometry in
+        
+            ZStack {
+                Color("BackgroundColor").ignoresSafeArea()
+                ScrollView{
+                    TeamBox(teamName: "Le bureau", imageName: "Team", teamText: description[0],geometry: geometry)
+                    TeamBox(teamName: "BDI", imageName: "Team", teamText: description[1],geometry: geometry)
+                }
+            }
+        }
     }
 }
 
@@ -18,3 +28,36 @@ struct TeamsView_Previews: PreviewProvider {
         TeamsView()
     }
 }
+
+struct TeamBox : View{
+    let teamName : String
+    let imageName : String
+    let teamText : String
+    let geometry : GeometryProxy
+    var body: some View {
+        
+            VStack(){
+                HStack{
+                    Rectangle().frame(width: geometry.size.width*0.3, height: 1)
+                    Spacer()
+                    Text(teamName).font(.headline)
+                    Spacer()
+                    Rectangle().frame(width: geometry.size.width*0.3, height: 1)
+                }
+                HStack{
+                    Image(imageName).resizable().scaledToFit()
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke())
+                        .frame(width: geometry.size.width*0.45).fixedSize(horizontal: false, vertical: true)
+                    Text(teamText).font(.body).fixedSize(horizontal: false, vertical: true).frame(width: geometry.size.width*0.45)
+                }
+            }.padding(10)
+        
+    }
+    
+}
+
+
+let description : [String] = [
+"Le bureau Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet blandit egestas. Nunc odio elit, maximus vitae volutpat non, porta nec orci. Integer est odio, bibendum ut lorem id, varius imperdiet ipsum. ",
+    "Le BDI, les meilleurs, sans me vanter bien sur"
+]
